827065fb441891ddb19f537d666796b2bd82903b
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2024-03-16 08:37:07 -0500

    Removed syntax

--- Bayesian Statistics Sticker.R
+++ Bayesian Statistics Sticker.R
@@ -2,51 +2,8 @@ library(hexSticker)
 library(ggplot2)
 library(dplyr)
 library(bayesrules)
-library(survival)
-library(forecast)
 
 set.seed(1234)
-
-# Causal Inference Sticker
-### Create the Graphic
-# Create the dataset for group "A"
-time <- 1:20
-group_id_A <- rep("A", 20)
-outcome_A <- c(rnorm(7, mean = 25, sd = 0.1), seq(25, 35, length.out = 13) + rnorm(13, sd = 0.4))
-
-# Create the dataset for group "B" with steadily decreasing values after time period 7
-group_id_B <- rep("B", 20)
-outcome_B <- c(rnorm(7, mean = 25, sd = 0.1), seq(25, 15, length.out = 13) + rnorm(13, sd = 0.4))
-
-# Combine data into a data frame
-simulated_data <- data.frame(
-  time = rep(time, 2),
-  group_id = c(group_id_A, group_id_B),
-  Y = c(outcome_A, outcome_B)
-)
-
-synth <- ggplot(simulated_data, aes(x = time, y = Y, color = group_id, linetype = group_id)) +
-  geom_line(size = 1.15) +
-  geom_vline(xintercept = 7, linetype = "22", color = "#440154", size = 1) +
-  scale_color_manual(values = c("A" = "#30b57b", "B" = "#33648d")) +
-  scale_linetype_manual(values = c("A" = "solid", "B" = "solid")) +
-  theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        panel.background = element_rect(fill = "#dfe7f2"),
-        panel.grid.major = element_line(color = alpha("#440154", 0.3), linetype = "solid", size = 0.5),
-        panel.grid.minor = element_line(color = alpha("#440154", 0.3), linetype = "solid", size = 0.5)
-  )
-
-### Create the Sticker
-sticker(synth, package = "", p_size = 20, s_x = 1, s_y = 1, 
-        s_width = 2.2, s_height = 2.35, h_fill = "#dfe7f2", h_color = "#440154",
-        white_around_sticker = TRUE,
-        filename = "C:/Users/blookabaugh/Desktop/test2.png")
 
 # Bayesian Statistics Sticker
 ### Create the Graphic
